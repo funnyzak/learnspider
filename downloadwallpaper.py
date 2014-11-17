81e929f099564f415140ebe58e626796b052c7c6
fw <fw@localhost.localdomain>
2014-11-17 07:26:28 -0800

    rewrite the python program to class

--- downloadwallpaper.py
+++ downloadwallpaper.py
@@ -1,13 +1,40 @@
 #-*-coding: utf-8 -*-
+
 import urllib2
 import re
 import urllib
+import os
+import logging
 
-urlpage = "http://cn.bing.com"
-indexpage = urllib2.urlopen(urlpage)
-pagecontent =  indexpage.read()
-rule = "http\S*\.jpg"
-pattern = re.compile(rule)
-matchimg = pattern.search(pagecontent)
-imageurl = matchimg.group()
-urllib.urlretrieve(imageurl,'bing.jpg')
+___author___ = "fangwei"
+
+class downBing(object):
+	"""
+	download 'cn.bing.com' wallpaper
+	"""
+	def __init__(self,urlpath,filepath):
+		self.urlpath = urlpath
+		self.filepath = filepath
+		self.rule  =re.compile("http\S*\.jpg")
+	def start(self)	:
+		urlhandler = urllib2.urlopen(self.urlpath)
+		pageContent = urlhandler.read()
+		pattern= self.rule.search(pageContent)
+		imageurl = pattern.group()
+		urllib.urlretrieve(imageurl,'bing.jpg')
+
+if __name__ == '__main__':
+
+	downbing = downBing("http://cn.bing.com","os.getcwd()")
+	logging.warning("DownLoading......starting......")
+	downbing.start()
+	print "we have download the image"
+		
+#rlpage = "http://cn.bing.com"
+#ndexpage = urllib2.urlopen(urlpage)
+#pagecontent =  indexpage.read()
+#rule = "http\S*\.jpg"
+#pattern = re.compile(rule)
+#matchimg = pattern.search(pagecontent)
+#imageurl = matchimg.group()
+#urllib.urlretrieve(imageurl,'bing.jpg')
