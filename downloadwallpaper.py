6c03044b5e1510d591d1907e9035a5a5672b0a54
fw <fw@localhost.localdomain>
2014-11-19 08:01:18 -0800

    set param

--- downloadwallpaper.py
+++ downloadwallpaper.py
@@ -6,6 +6,8 @@ import urllib
 import os
 import logging
 import time
+import sys
+
 ___author___ = "fangwei"
 
 class downBing(object):
@@ -15,22 +17,45 @@ class downBing(object):
 	def __init__(self,urlpath,filepath):
 		self.urlpath = urlpath
 		self.filepath = filepath
-		self.rule  =re.compile("http\S*\.jpg")
+		self.rulepng = re.compile("http\S*\.png")
+		self.rulejpg = re.compile("http\S*\.jpg")
+		self.rulegif = re.compile("http\S*\.png")
+		self.count = 1
 	def start(self)	:
 		urlhandler = urllib2.urlopen(self.urlpath)
 		pageContent = urlhandler.read()
-		pattern= self.rule.search(pageContent)
-		imageurl = pattern.group()
-		print imageurl
-		urllib.urlretrieve(imageurl,str(time.strftime("%Y-%b-%d-%a-%H-%M-%S",time.localtime())+".jpg"))
+		pattern= self.rulepng.findall(pageContent)
+		print pattern.__len__()
+		length = pattern.__len__()
+		if length > 1:
+			for imageurl in pattern:
+				print imageurl
+				self.count = self.count+1
+				urllib.urlretrieve(imageurl,str(time.strftime("%Y-%b-%d-%a-%H-%M-%S",time.localtime())+"-"+str(self.count)+".png"))
+		
+		pattern = self.rulejpg.findall(pageContent)
+		length = pattern.__len__()
+		if length > 1:
+			for imageurl in pattern:
+				print imageurl
+				self.count = self.count+1
+				urllib.urlretrieve(imageurl,str(time.strftime("%Y-%b-%d-%a-%H-%M-%S",time.localtime())+"-"+str(self.count)+".jpg"))
+		pattern = self.rulegif.findall(pageContent)
+		length = pattern.__len__()
+		if length > 1:
+			for imageurl in pattern:
+				print imageurl
+				self.count = self.count+1
+				urllib.urlretrieve(imageurl,str(time.strftime("%Y-%b-%d-%a-%H-%M-%S",time.localtime())+"-"+str(self.count)+"git"))
 
 if __name__ == '__main__':
-
-	downbing = downBing("http://cn.bing.com","os.getcwd()")
-	logging.warning("DownLoading......starting......")
-	downbing.start()
-	print "we have download the image"
-		
+	len = len(sys.argv)
+	if len == 2:
+		downbing = downBing(sys.argv[1],"os.getcwd()")
+		logging.warning("DownLoading......starting......")
+		downbing.start()
+		print "we have download the image"
+		exit()
 #urlpage = "http://cn.bing.com"
 #indexpage = urllib2.urlopen(urlpage)
 #pagecontent =  indexpage.read()
@@ -38,4 +63,4 @@ if __name__ == '__main__':
 #pattern = re.compile(rule)
 #matchimg = pattern.search(pagecontent)
 #imageurl = matchimg.group()
-#urllib.urlretrieve(imageurl,'bing.jpg')
+#urllib.urlretrieve(imageurl,'biing.jpg')
