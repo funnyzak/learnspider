c98ef3e1e287e1a57e3be50420ccb1b44e6ff980
fw <fw@localhost.localdomain>
2014-11-23 00:20:10 -0800

    add http header

--- downloadwallpaper.py
+++ downloadwallpaper.py
@@ -7,23 +7,31 @@ import os
 import logging
 import time
 import sys
-
+import cookielib
 ___author___ = "fangwei"
 
 class downBing(object):
 	"""
 	download 'cn.bing.com' wallpaper
 	"""
-	def __init__(self,urlpath,filepath):
+	def __init__(self,urlpath,filepath,data):
 		self.urlpath = urlpath
 		self.filepath = filepath
 		self.rulepng = re.compile('''(http://[^\{\;\,\<\"]*\.png)\w*?''')
 		self.rulejpg = re.compile('''(http://[^\{\;\,\<\"]*\.jpg)\w*?''')
 		self.rulegif = re.compile('''(http://[^\{\;\,\<\"]*\.gif)\w*?''')
 		self.count = 1
+		self.data = data
 	def start(self)	:
-		urlhandler = urllib2.urlopen(self.urlpath)
-		pageContent = urlhandler.read()
+		#use handle method
+		headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; rv:33.0) Gecko/20100101 Firefox/33.0'}
+		request = urllib2.Request(self.urlpath)
+		request.add_header('User-Agent',headers['User-Agent'])
+		cookie = cookielib.CookieJar()
+		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+		response = opener.open(request)
+		#urlhandler = urllib2.urlopen(self.urlpath)
+		pageContent = response.read()
 		pageContent = pageContent.replace(r"\/","/")
 		pattern= self.rulepng.findall(pageContent)
 		print pattern.__len__()
@@ -51,8 +59,11 @@ class downBing(object):
 
 if __name__ == '__main__':
 	len = len(sys.argv)
-	if len == 2:
-		downbing = downBing(sys.argv[1],"os.getcwd()")
+	if len >= 2:	
+		if len==2:
+			downbing = downBing(sys.argv[1],"os.getcwd()",{'URL':'URL'})
+		if len==3:
+			downbing = downBing(sys.argv[1],"os.getcwd()",sys.argv[2])
 		logging.warning("DownLoading......starting......")
 		downbing.start()
 		print "we have download the image"
